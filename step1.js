bcd26cb3b9fda5bdae03e8a692800bfb837924d9
RiptideWallace <brysonc1987@gmail.com>
2017-07-03 18:37:48 +0000

    HTTP Request Exercise Step 1

---
+++ step1.js
@@ -0,0 +1,28 @@
+function getAndPrintHTMLChunks () {
+
+  var https = require('https')
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function (response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      console.log('Chunk Recieved. Length: ', data.length);
+      console.log(data.toString());
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete');
+    });
+
+  });
+
+}
+getAndPrintHTMLChunks()
+
+
