be6e551c6e8f19aa9f94b4e0bbbd886d21be3c30
RiptideWallace <brysonc1987@gmail.com>
2017-07-03 19:11:28 +0000

    HTTP Request Step 2 Complete

---
+++ step2.js
@@ -0,0 +1,32 @@
+function getAndPrintHTML () {
+
+  var https = require('https');
+  var buf = "";
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+console.log('Making a GET Request to the Server');
+https.get(requestOptions, function (response) {
+
+    response.setEncoding('utf8');
+    console.log('The data is available');
+
+    response.on('data', function (chunk) {
+      buf += chunk.toString()
+      console.log('Chunk Recieved. Length: ', chunk.length);
+
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete');
+      console.log(buf);
+    });
+
+  });
+
+}
+
+getAndPrintHTML();
